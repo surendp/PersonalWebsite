02e8b7e9cc9402047d05b3d8693e672cee3ad030
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-06-24 23:22:34 +0300

    load project list from JSON file.

--- src/app/components/projectList.js
+++ src/app/components/projectList.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import ProjectListItem from './projectListItem';
 
 import '../../styles/project-list.css';
+
+import PersonalWebsite from '../../images/portfolio.png';
 import HimalayanKitchen from '../../images/himalayan-kitchen-frontpage.png';
 import BoardGame from '../../images/board-game.png';
 import MineSweeper from '../../images/minesweeper.png';
@@ -15,29 +17,54 @@ class ProjectList extends Component{
     super(props);
 
     this.state = {
-      projectList:[ { name:"Himalayan Kitchen",description:"A restaurant website",
-                      image: { url: HimalayanKitchen}
+      projectList:[ { name:"Personal Website",description:"A website for displaying personal information.",
+                      image: { url: PersonalWebsite},
+                      url: {githubLink: "https://github.com/surendp/PersonalWebsite",
+                            projectLink: "http://localhost:8080/"
+                          }
+                    },
+                    { name:"Himalayan Kitchen",description:"A restaurant website",
+                      image: { url: HimalayanKitchen},
+                      url: {githubLink: "https://github.com/surendp/himalayankitchen",
+                            projectLink: "https://secret-castle-23220.herokuapp.com"
+                          }
                     },
                     {name:"Board Game", description:"A web Game",
-                      image: { url: BoardGame}
+                      image: { url: BoardGame},
+                      url: {githubLink: "https://github.com/surendp/",
+                        projectLink: "http://users.metropolia.fi/~surendp/BoardGameCanvasOopJqueryVersion_0.1/FirstPage/"
+                      }
                     },
                     {name:"MineSweeper", description:"A console Game",
-                      image: { url: MineSweeper}
+                      image: { url: MineSweeper},
+                      url: {githubLink: "https://github.com/surendp/Minesweeper", projectLink: "#"}
                     },
                     {name:"Voting App", description:"A mini project",
-                      image: { url: VotingApp}
+                      image: { url: VotingApp},
+                      url: {githubLink: "https://github.com/surendp/VotingApp", projectLink: "#"}
                     },
                     {name:"Youtube Videos Player", description:"An application to play youtube videos",
-                      image: { url: YoutubeVideoPlayer}
+                      image: { url: YoutubeVideoPlayer},
+                      url: {githubLink: "https://github.com/surendp/YoutubeVideosStreamingApp", projectLink: "#"}
                     },
                   ]
     };
 
   }
 
+
+  componentDidMount(){
+    fetch('./projects.json')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({projectList: data.projects})
+      })
+  }
+
   render(){
     const projects = this.state.projectList.map((project) => (
         <ProjectListItem   key = {project.name}
+                            url = {PersonalWebsite}
                             project ={project} />
     ));
 
